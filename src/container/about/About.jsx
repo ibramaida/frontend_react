0ef9b2d67c78cdd80465fe526dbea4bf7bbd777c
Ibrahim Maida <ibrahim@o365.tijdelijkehosting.nl>
2022-02-20 14:04:59 +0100

    added about connection to sanity, get data dynamic

--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 
+import { urlFor, client } from "../../client";
+
 import "./About.scss";
 
-const abouts = [
-  {
-    title: "Web Developer",
-    description: "I,m a good developer",
-    imageURL: images.about01,
-  },
-  {
-    title: "Web Designer",
-    description: "I,m a good Designer",
-    imageURL: images.about02,
-  },
-  { title: "UI/UX", description: "I,m a good UI/UX", imageURL: images.about03 },
-  {
-    title: "Web Animations",
-    description: "I make wonderful animation",
-    imageURL: images.about04,
-  },
-];
+// const abouts = [
+//   {
+//     title: "Web Developer",
+//     description: "I,m a good developer",
+//     imageURL: images.about01,
+//   },
+//   {
+//     title: "Web Designer",
+//     description: "I,m a good Designer",
+//     imageURL: images.about02,
+//   },
+//   { title: "UI/UX", description: "I,m a good UI/UX", imageURL: images.about03 },
+//   {
+//     title: "Web Animations",
+//     description: "I make wonderful animation",
+//     imageURL: images.about04,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
   return (
     <>
       <h2 className="head-text">
@@ -42,7 +53,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imageURL} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
